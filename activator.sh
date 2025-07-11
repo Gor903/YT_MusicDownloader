ced4af4cb99afb24a5d83cca7861c760209ed2fb
Gor903 <Gor.Beglaryan.RW@gmail.com>
2025-07-11 19:15:06 +0400

    _

--- activator.sh
+++ activator.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SCRIPT_DIR="$(pwd)"
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 source $SCRIPT_DIR/venv/bin/activate
 python $SCRIPT_DIR/main.py $1 $SCRIPT_DIR
@@ -8,7 +8,7 @@ file=$(find $SCRIPT_DIR/temp -type f)
 
 ffmpeg -i "$file" -metadata Container=""  "/home/$(whoami)/Music/$(basename "$file")"
 
-rm -rf $file
+rm -rf "$SCRIPT_DIR/temp/$(basename "$file")"
 
 exit
 
