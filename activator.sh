1d1fc5fed5d041190d5b3b8a76a720da18c7c161
Gor903 <Gor.Beglaryan.RW@gmail.com>
2025-07-11 19:06:21 +0400

    Update

--- activator.sh
+++ activator.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+SCRIPT_DIR="$(pwd)"
 
 source $SCRIPT_DIR/venv/bin/activate
-python $SCRIPT_DIR/main.py $1
+python $SCRIPT_DIR/main.py $1 $SCRIPT_DIR
 file=$(find $SCRIPT_DIR/temp -type f)
+
 ffmpeg -i "$file" -metadata Container=""  "/home/$(whoami)/Music/$(basename "$file")"
-rm -rf "$SCRIPT_DIR/temp/$(basename "$file")"
+
+rm -rf $file
+
 exit
 
--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import yt_dlp
 import sys
 
+print(__file__)
+
 def download_audio(url):
     ydl_opts = {
         "format": "bestaudio/best",
         "extract_audio": True,  
         "audio_format": "mp3",  
-        "outtmpl": "/home/gor/Desktop/.PastProjects/python/YT_MusicDownloader/temp/%(title)s.mp3",
+        "outtmpl": f"{path}/temp/%(title)s.mp3",
     }
     
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
 
 video_url = sys.argv[1]
+path = sys.argv[2]
 download_audio(video_url)
